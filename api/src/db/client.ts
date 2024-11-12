664fe180fd986e9e4f478b4602d036e423e14996
scyanth <34488277+scyanth@users.noreply.github.com>
2024-11-12 16:25:17 +0100

    fix dockerfile 2

--- api/src/db/client.ts
+++ api/src/db/client.ts
@@ -2,6 +2,11 @@ import { DataSource } from "typeorm";
 import { Repo } from "../entities/repo";
 import { Status } from "../entities/status";
 import { Lang } from "../entities/lang";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
+  process.env;
 
 // export const dataSource = new DataSource({
 //     type: "sqlite",
@@ -10,13 +15,24 @@ import { Lang } from "../entities/lang";
 //     synchronize: true
 // });
 
+// export const dataSource = new DataSource({
+//   type: "postgres",
+//   host: "db", // Nom de l'image associé à Postgres --name dans la commande
+//   port: 5432,
+//   username: "postgres",
+//   password: "password",
+//   database: "postgres",
+//   entities: [Repo, Status, Lang],
+//   synchronize: true,
+// });
+
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "db", // Nom de l'image associé à Postgres --name dans la commande
+  host: POSTGRES_HOST, // Nom de l'image associé à Postgres --name dans la commande
   port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "postgres",
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
   entities: [Repo, Status, Lang],
   synchronize: true,
 });
